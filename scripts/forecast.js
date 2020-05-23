d94a7a488781e31d56e71acc945ff44bf296f400
RakeshPugazh <58099489+RakeshPugazh@users.noreply.github.com>
2020-05-23 18:12:28 +0530

    Update forecast.js

--- scripts/forecast.js
+++ scripts/forecast.js
@@ -4,7 +4,7 @@ const apikey = 'ue8hpAwN27Hb9zbaBG4hSD0LXUVb9LO8' ;
 
 const getWeather = async(Key)=>{
 
- const url = 'http://dataservice.accuweather.com/currentconditions/v1/' + Key + '?apikey=' + apikey;
+ const url = 'https://dataservice.accuweather.com/currentconditions/v1/' + Key + '?apikey=' + apikey;
  
  const response =await fetch(url);
 
@@ -19,7 +19,7 @@ const getWeather = async(Key)=>{
 
 const getCity = async (city)=>{
  //const proxy = 'https://cors-anywhere.herokuapp.com/';
- const base = 'http://dataservice.accuweather.com/locations/v1/cities/search' ; 
+ const base = 'https://dataservice.accuweather.com/locations/v1/cities/search' ; 
  const query = '?apikey=' + apikey + '&q=' + city ;
    
   const url = base + query ;
@@ -35,18 +35,7 @@ const getCity = async (city)=>{
 
 
 
- /*
- 	getCity('chennai')
-.then(data => {
-	
-     return getWeather(data.Key)
-    })
-  .then(data=>{
-  	 const condition = data.Temperature.Metric.Value + 'C'
-     console.log(condition);
-  
-})
- */
+ 
 
 
 
